10fe55927d911ec775c9dec5a0410dddb75ee564
Berkay OCER <berkayocer2003@gmail.com>
2024-03-06 18:11:11 +0300

    a

---
+++ c#/2.5_Arrays/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] x = {10,20};
+int[] y = x;
+
+System.Console.WriteLine(x[0]);
+System.Console.WriteLine(y[0]);
+
+x[0]=20;
+
+System.Console.WriteLine(x[0]);
+System.Console.WriteLine(y[0]);
