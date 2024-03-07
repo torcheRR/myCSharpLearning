5889a88653d33f94f704b2c2d950178e1538aa60
Berkay OCER <berkayocer2003@gmail.com>
2024-03-07 16:10:12 +0300

    .

---
+++ c#/7.0_For/7.0_For/Program.cs
@@ -0,0 +1,6 @@
+﻿// For
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("Yaren'i Çok Seviyorum.");
+}
---
+++ c#/7.1_For/7.1_For/Program.cs
@@ -0,0 +1,11 @@
+﻿// for uygulamları
+
+//1- 1 den 100 e kadar olan sayıların toplamını yazdırınız
+
+int toplam =0;
+for (int i = 0; i <= 100; i++)
+{
+    toplam += i;
+}
+
+Console.WriteLine(toplam);
