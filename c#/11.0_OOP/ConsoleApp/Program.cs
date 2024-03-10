059926f8bb5f8fc42792f4c72efc80dd3d9b2c32
Berkay OCER <berkayocer2003@gmail.com>
2024-03-10 10:11:36 +0300

    init

---
+++ c#/11.0_OOP/ConsoleApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp{
+    class Program
+    {
+        static void Main(string[] args){
+
+            //class => object (ogr1, ogr2)
+
+            Ogrenci ogr1 = new Ogrenci(){   AdSoyad = "Berkay Öcer",  Sube = "102",   OgrenciNo = "2103013260"    };
+            Ogrenci ogr2 = new Ogrenci(){   AdSoyad = "Yaren Öcer",  Sube = "102",   OgrenciNo = "2103013225"    };
+            Ogrenci ogr3 = new Ogrenci(){   AdSoyad = "Gökay Öcer",  Sube = "5/B",   OgrenciNo = "123"    };
+
+            Ogrenci[] ogrenciler = new Ogrenci[3]{ogr1,ogr2,ogr3};
+
+            foreach (var ogrenci in ogrenciler){
+                System.Console.WriteLine(ogrenci.BilgileriYazdır());
+            }
+
+        }
+    }
+
+    class Ogrenci{
+        //peoperty => string, int
+        
+        public string OgrenciNo { get; set; }
+        public string AdSoyad { get; set; }
+        public string Sube { get; set; }
+
+        //methods
+
+        // public void BilgileriYazdır(){
+        //     System.Console.WriteLine($"{this.OgrenciNo} numaralı öğrencinin adı {this.AdSoyad} ve şubesi {this.Sube}");
+        // }
+
+        public string BilgileriYazdır(){
+            return $"{this.OgrenciNo} numaralı öğrencinin adı {this.AdSoyad} ve şubesi {this.Sube}";
+        }
+
+
+    }
+}
+
+//ConsoleApp => Program, Ogrenci
+//System => Console
+//System.IO => File, Directort, Path
---
+++ c#/11.1_OOP/ConsoleApp/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var soru1 = new Soru(1, "Hangisi programlama dili değildir?", new string[] { "Pyhton", "C#", "Java", "Html" }, "Html");
+            var soru2 = new Soru(2, "Hangisi en popüler programlama dilidir?", new string[] { "Pyhton", "C#", "Java", "Html" }, "c#");
+            var soru3 = new Soru(3, "Hangisi en popüler web programlama platformudur?", new string[] { "Pyhton", "ASP.Net", "Django", "Spring" }, "Python");
+
+            System.Console.WriteLine(soru1.soruYazdir());
+            System.Console.WriteLine(soru1.cevapKontrol(Console.ReadLine()));
+
+            System.Console.WriteLine(soru2.soruYazdir());
+            System.Console.WriteLine(soru2.cevapKontrol(Console.ReadLine()));
+
+            System.Console.WriteLine(soru3.soruYazdir());
+            System.Console.WriteLine(soru3.cevapKontrol(Console.ReadLine()));
+
+        }
+
+    }
+
+
+        // protected, internal, protected internal, private protected
+    class Soru
+    {
+        public Soru(int soruID, string soruMetni, string[] secenekler, string cevap)
+        {
+            this.soruID = soruID;
+            this.soruMetni = soruMetni;
+            this.secenekler = secenekler;
+            this.cevap = cevap;
+        }
+
+        //properties
+        private int soruID { get; set; }
+        private string soruMetni { get; set; }
+        private string[] secenekler { get; set; }
+        private string cevap { get; set; }
+
+
+        public string soruYazdir()
+        {
+            string soru = "\n";
+            soru += this.soruMetni + "\n";
+
+            foreach (var secenek in this.secenekler)
+            {
+                soru += secenek + "\n";
+            }
+            return soru;
+        }
+
+        //methods
+        public bool cevapKontrol(string cevap)
+        {
+            return this.cevap.ToLower() == cevap.ToLower();
+        }
+    }
+}
