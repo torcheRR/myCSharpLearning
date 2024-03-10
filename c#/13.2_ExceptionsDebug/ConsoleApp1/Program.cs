705b63ee394d6052611d36cd066b46880fa98419
Berkay OCER <berkayocer2003@gmail.com>
2024-03-10 14:12:38 +0300

    init

--- c#/13.2_ExceptionsDebug/ConsoleApp1/Program.cs
+++ c#/13.2_ExceptionsDebug/ConsoleApp1/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            
+            var sayi1=10;
+            var sayi2 = 20;
+
+
+            var sonuc = Toplam(sayi1,sayi2);
+
+            System.Console.WriteLine(sonuc);
+        }
+
+        static int Toplam(int a , int b){
+            return a+b;
         }
     }
 }
--- c#/13.2_ExceptionsDebug/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ c#/13.2_ExceptionsDebug/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+990fd3679efed91356b201bce5530ec6635ebbdc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e971e37afd3e4577b86f47dbd344186cef598de3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
