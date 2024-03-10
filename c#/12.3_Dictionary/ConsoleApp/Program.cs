26ffbf8d8f59f5c12a19a66be4728095d12342f2
Berkay OCER <berkayocer2003@gmail.com>
2024-03-10 13:13:56 +0300

    i

---
+++ c#/12.3_Dictionary/ConsoleApp/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ConsoleApp{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            //dictionary
+
+            //key-value==> plaka - şehir
+
+            Dictionary<int,string> plakalar = new Dictionary<int, string>();
+
+            plakalar.Add(58,"Sivas");
+            plakalar.Add(34,"İstanbul");
+            plakalar.Add(28,"Giresun");
+
+
+            Dictionary<int,string> sayilar = new Dictionary<int, string>(){
+                {1,"Bir"},
+                {2,"İki"},
+                {3,"Üç"}
+            };
+
+            // System.Console.WriteLine(plakalar[28]);
+
+            if(plakalar.ContainsKey(61)){
+                System.Console.WriteLine(plakalar[61]);
+            }
+
+
+            foreach(var plaka in plakalar){
+                System.Console.WriteLine(plaka.Key + " " + plaka.Value);
+            }
+
+
+            //update 
+            sayilar[1] = "one";
+            // sayilar.Remove(1);
+            //sayilar.Clear();
+
+            System.Console.WriteLine(sayilar[1]);
+
+        }
+    }
+}
