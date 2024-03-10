990fd3679efed91356b201bce5530ec6635ebbdc
Berkay OCER <berkayocer2003@gmail.com>
2024-03-10 13:22:43 +0300

    i

---
+++ c#/13.0_Exceptions/ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleApp{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            //Exception
+            //System.FormatException
+            //System.DivideByZeroException
+
+            System.Console.Write("1. Sayı :");
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+            System.Console.Write("2. Sayı :");
+            int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            var sonuc = sayi1/sayi2;
+
+            Urun urun= new Urun();
+            System.Console.WriteLine(urun.urunAdi);
+
+
+
+            //Exception Handling
+        }
+    }
+
+
+    class Urun{
+
+        public string urunAdi { get; set;}
+
+    }
+}
