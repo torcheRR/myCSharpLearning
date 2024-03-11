eb8110bd32b444948be0abb862fc3ec193c61b44
Berkay OCER <berkayocer2003@gmail.com>
2024-03-11 06:19:52 +0300

    i

--- c#/12.3_Dictionary/ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ c#/12.3_Dictionary/ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72a7e2937e24e6eaae0e70d13c7bbe5fe027cffc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+705b63ee394d6052611d36cd066b46880fa98419")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- c#/13.2_ExceptionsDebug/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ c#/13.2_ExceptionsDebug/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e971e37afd3e4577b86f47dbd344186cef598de3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+705b63ee394d6052611d36cd066b46880fa98419")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
